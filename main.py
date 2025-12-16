7e84697e3dcb1990a07db921aa65f2a63e507eb9
Aashique Karn <aashiquekarn@gmail.com>
2025-12-16 10:56:47 +0530

    master | initial commit | adding structured llm

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from dotenv import load_dotenv
 from langchain import hub
 from langchain.agents import AgentExecutor
 from langchain.agents.react.agent import create_react_agent
-from langchain_core.output_parsers.pydantic import PydanticOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnableLambda
 from langchain_openai import ChatOpenAI
@@ -17,12 +16,12 @@ tools = [TavilySearch()]
 llm = ChatOpenAI(model="gpt-4", temperature=0)
 react_prompt = hub.pull("hwchase17/react")
 
-output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+structured_llm = llm.with_structured_output(AgentResponse)
 
 react_prompt_with_format_instructions = PromptTemplate(
     template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
     input_variables=["input", "agent_scratchpad", "tool_names"],
-).partial(format_instructions=output_parser.get_format_instructions())
+).partial(format_instructions="")
 
 
 agent = create_react_agent(
@@ -33,8 +32,7 @@ agent = create_react_agent(
 
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 extract_output = RunnableLambda(lambda x: x["output"])
-parse_output = RunnableLambda(lambda x: output_parser.parse(x))
-chain = agent_executor | extract_output | parse_output
+chain = agent_executor | extract_output | structured_llm
 
 def main():
     result = chain.invoke({"input": "Search for 3 job postings for an ai engineer using langchain in the bay area on the linked and list their title with salary and link to the posting."})
